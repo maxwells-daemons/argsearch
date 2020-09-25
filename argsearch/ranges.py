b25ba12cb58d3f5d08bef009864f60c320e4ee02
maxwells-daemons <aidanswope@gmail.com>
2020-09-24 23:35:00 -0700

    Add log-uniform int and float ranges

--- argsearch/ranges.py
+++ argsearch/ranges.py
@@ -4,10 +4,40 @@ Defines Ranges, which specify how to sample arguments of various types.
 
 import abc
 import argparse
-from typing import List
+from typing import List, Union
+import numbers
 import random
 
 import numpy as np
+from scipy import stats
+
+
+def cast_range_argument(arg: str) -> Union[int, float, str]:
+    """
+    Determine whether a range argument is integral, floating-point, or categorical.
+
+    Parameters
+    ----------
+    arg
+        The argument to check.
+
+    Returns
+    -------
+    int
+        If `arg` is castable to an integer.
+    float
+        If `arg` is castable to floating-point, but not an integer.
+    string
+        If `arg` is not castable to a numeric type.
+    """
+
+    try:
+        return int(arg)
+    except ValueError:
+        try:
+            return float(arg)
+        except ValueError:
+            return arg
 
 
 class Range(abc.ABC):
@@ -50,9 +80,9 @@ class IntRange(Range):
     A range of integral values between a minimum and a maximum.
     """
 
-    def __init__(self, min_value: int, max_value: int):
-        self.min_value = min_value
-        self.max_value = max_value
+    def __init__(self, a: int, b: int):
+        self.min_value = min(a, b)
+        self.max_value = max(a, b)
 
     def random_sample(self) -> str:
         return str(random.randint(self.min_value, self.max_value))
@@ -63,20 +93,58 @@ class IntRange(Range):
         return list(map(str, space))
 
 
+class LogIntRange(Range):
+    """
+    A log-uniform range of integral values between a minimum and a maximum.
+    """
+
+    def __init__(self, a: int, b: int):
+        self.min_value = min(a, b)
+        self.max_value = max(a, b)
+        self.distribution = stats.loguniform(self.min_value, self.max_value)
+
+    def random_sample(self) -> str:
+        sample = int(self.distribution.rvs(1).item())
+        return str(sample)
+
+    def grid(self, divisions: int) -> List[str]:
+        divisions = min(divisions, self.max_value - self.min_value + 1)
+        space = np.geomspace(self.min_value, self.max_value, num=divisions, dtype=int)
+        return list(map(str, space))
+
+
 class FloatRange(Range):
     """
     A range of floating-point values between a minimum and a maximum.
     """
 
-    def __init__(self, min_value: float, max_value: float):
-        self.min_value = min_value
-        self.max_value = max_value
+    def __init__(self, a: float, b: float):
+        self.min_value = min(a, b)
+        self.max_value = max(a, b)
 
     def random_sample(self) -> str:
         return str(random.uniform(self.min_value, self.max_value))
 
     def grid(self, divisions: int) -> List[str]:
         space = np.linspace(self.min_value, self.max_value, num=divisions, dtype=float)
+        return list(map(str, space))
+
+
+class LogFloatRange(Range):
+    """
+    A log-uniform range of floating-point values between a minimum and a maxmimum.
+    """
+
+    def __init__(self, a: float, b: float):
+        self.min_value = min(a, b)
+        self.max_value = max(a, b)
+        self.distribution = stats.loguniform(self.min_value, self.max_value)
+
+    def random_sample(self) -> str:
+        return str(self.distribution.rvs(1).item())
+
+    def grid(self, divisions: int) -> List[str]:
+        space = np.geomspace(self.min_value, self.max_value, num=divisions, dtype=float)
         return list(map(str, space))
 
 
@@ -110,24 +178,27 @@ class TemplateRange(argparse.Action):
             )
 
         if len(values) == 2:
-            try:
-                value_1 = int(values[0])
-                value_2 = int(values[1])
-                min_value = min(value_1, value_2)
-                max_value = max(value_1, value_2)
-                setattr(namespace, self.dest, IntRange(min_value, max_value))
-                return
-            except ValueError:
-                pass
+            first = cast_range_argument(values[0])
+            second = cast_range_argument(values[1])
 
-            try:
-                value_1 = float(values[0])
-                value_2 = float(values[1])
-                min_value = min(value_1, value_2)
-                max_value = max(value_1, value_2)
-                setattr(namespace, self.dest, FloatRange(min_value, max_value))
+            if isinstance(first, int) and isinstance(second, int):
+                setattr(namespace, self.dest, IntRange(first, second))
                 return
-            except ValueError:
-                pass
+
+            if isinstance(first, numbers.Number) and isinstance(second, numbers.Number):
+                setattr(namespace, self.dest, FloatRange(first, second))
+                return
+
+        if len(values) == 3 and values[0] == "LOG":
+            first = cast_range_argument(values[1])
+            second = cast_range_argument(values[2])
+
+            if isinstance(first, int) and isinstance(second, int):
+                setattr(namespace, self.dest, LogIntRange(first, second))
+                return
+
+            if isinstance(first, numbers.Number) and isinstance(second, numbers.Number):
+                setattr(namespace, self.dest, LogFloatRange(first, second))
+                return
 
         setattr(namespace, self.dest, CategoricalRange(values))
